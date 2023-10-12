6cb957b5d8f6aac7f21f53b11cb60af6ed4f37a5
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-10-12 17:02:47 +0100

    update

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  {
+    id: 1,
+    description: "Passport",
+    quantity: 12,
+    packed: false,
+  },
+  {
+    id: 1,
+    description: "Socks",
+    quantity: 12,
+    packed: false,
+  },
 
+];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />
     </div>
   );
 }
 
+function Logo() {
+  return (
+    <div className=" flex items-center justify-center font-[nunito] font-bold text-sm text-[#f5f8f9] bg-[#363535] text-center h-[10svh]">
+      <h1> Far Away 💼</h1>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <div className="flex items-center justify-center font-[nunito] bg-white h-[20svh] text-sm">
+      <p className="">What do you need for your trip 🤔? </p>
+    </div>
+  );
+}
+
+function PackagingList() {
+  return (
+    <div className="flex items-center justify-center font-[nunito] bg-[#f0f3f4] h-[60svh] text-sm">
+      LIST
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <div className="flex items-center justify-center font-[nunito] bg-[#363535] text-white h-[10svh]">
+      <h1>
+        <em>You have X items on your list, you have already packed X (X%)</em>
+      </h1>
+    </div>
+  );
+}
 export default App;
