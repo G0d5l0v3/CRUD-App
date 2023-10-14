4242083b7c41b8929382a2ed7b6dc3613cce5707
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-10-14 17:40:26 +0100

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const initialItems = [
   {
     id: 1,
@@ -6,7 +8,13 @@ const initialItems = [
     packed: false,
   },
   {
-    id: 1,
+    id: 2,
+    description: "Socks",
+    quantity: 5,
+    packed: true,
+  },
+  {
+    id: 3,
     description: "Socks",
     quantity: 5,
     packed: false,
@@ -26,17 +34,58 @@ function App() {
 
 function Logo() {
   return (
-    <div className=" flex items-center justify-center font-[nunito] font-bold text-sm text-[#f5f8f9] bg-[#363535] text-center h-[10svh]">
+    <div className="flex items-center justify-center font-[nunito] font-bold text-sm text-[#f5f8f9] bg-[#363535] text-center h-[10svh]">
       <h1> Far Away 💼</h1>
     </div>
   );
 }
 
 function Form() {
+  const [text, setText] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      text,
+      quantity,
+      packed: false,
+      id: Date.now()
+    }
+    setText("")
+    setQuantity(1)
+  }
+
   return (
-    <div className="flex items-center justify-center font-[nunito] bg-white h-[20svh] text-sm">
-      <p className="">What do you need for your trip 🤔? </p>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center justify-center gap-5 font-[nunito] bg-white h-[20svh] text-sm"
+    >
+      <p>What do you need for your trip 🤔? </p>
+      <select
+        className="bg-[#f0f3f4] px-3 py-1 rounded-lg"
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+          return (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          );
+        })}
+      </select>{" "}
+      <input
+        type="text"
+        placeholder="item..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="bg-[#f0f3f4] text-center rounded-lg py-1"
+      />
+      <button className="bg-[#363535] text-white px-3 py-1 rounded-md">
+        Add
+      </button>
+    </form>
   );
 }
 
@@ -44,11 +93,11 @@ function PackagingList() {
   return (
     <div>
       <ul
-        className="flex space-x-10 py
-      -[1rem] px-[10%] font-[nunito] bg-[#f0f3f4] h-[60svh] text-sm"
+        className="flex space-x-10 justify-center
+      py-[1rem] px-[10%] font-[nunito] bg-[#f0f3f4] h-[60svh] text-sm"
       >
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
@@ -56,10 +105,22 @@ function PackagingList() {
 }
 
 function Item({ item }) {
-  return (
+  const [isDeleted, setisDeleted] = useState(false);
+  const handleDelete = () => {
+    setisDeleted(true);
+  };
+
+  return !isDeleted ? (
     <li>
-      {item.quantity} {item.description} <button>❌</button>
+      <span className={item.packed ? "line-through" : ""}>
+        {item.quantity} {item.description}
+      </span>
+      <button className="pl-[1rem]" onClick={handleDelete}>
+        ❌
+      </button>
     </li>
+  ) : (
+    ""
   );
 }
 function Stats() {
